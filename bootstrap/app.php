2f5293972620bdec5b371286f03c15adeed18350
jibolashittubolu <54463012+jibolashittubolu@users.noreply.github.com>
2025-01-01 02:50:57 +0100

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,7 +30,7 @@ $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
     App\Http\Kernel::class
 );
-
+ 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
     App\Console\Kernel::class
